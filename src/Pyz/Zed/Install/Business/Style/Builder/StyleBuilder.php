9ede7534bee1eed6e16ee2d97bd541c65541ed55
vol4onok <alexander.veselov@spryker.com>
2023-12-14 09:57:06 +0200

    s

---
+++ src/Pyz/Zed/Install/Business/Style/Builder/StyleBuilder.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * MIT License
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace Pyz\Zed\Install\Business\Style\Builder;
+
+use Spryker\Zed\Install\Business\Style\Builder as SprykerStyleBuilder;
+class StyleBuilder extends SprykerStyleBuilder
+
+{
+    /**
+     * @var string
+     */
+    protected const OPTION_LOG = '11111';
+}
