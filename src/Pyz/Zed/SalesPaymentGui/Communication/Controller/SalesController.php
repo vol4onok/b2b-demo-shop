abee0956b31efa598758f05f56a4ee20b5364ac9
vol4onok <alexander.veselov@spryker.com>
2023-12-14 09:32:09 +0200

    a

---
+++ src/Pyz/Zed/SalesPaymentGui/Communication/Controller/SalesController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Pyz\Zed\SalesPaymentGui\Communication\Controller;
+
+use Spryker\Zed\SalesPaymentGui\Communication\Controller\SalesController as SprykerSalesController;
+class SalesController extends SprykerSalesController
+{
+    public function index()
+    {
+
+    }
+}
