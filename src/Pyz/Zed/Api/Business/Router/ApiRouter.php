c849dae3ca34ab2a546d8a16d61539db873b702a
vol4onok <alexander.veselov@spryker.com>
2023-12-14 09:46:44 +0200

    s

--- src/Pyz/Zed/Api/Business/Router/ApiRouter.php
+++ src/Pyz/Zed/Api/Business/Router/ApiRouter.php
@@ -3,6 +3,7 @@
 namespace Pyz\Zed\Api\Business\Router;
 
 use Spryker\Zed\Api\Business\RouterApiRouter as SprykerRouterApiRouter;
+
 class ApiRouter extends SprykerRouterApiRouter
 {
 
--- src/Pyz/Zed/SalesPaymentGui/Communication/Controller/SalesController.php
+++ src/Pyz/Zed/SalesPaymentGui/Communication/Controller/SalesController.php
@@ -3,6 +3,7 @@
 namespace Pyz\Zed\SalesPaymentGui\Communication\Controller;
 
 use Spryker\Zed\SalesPaymentGui\Communication\Controller\SalesController as SprykerSalesController;
+
 class SalesController extends SprykerSalesController
 {
     public function index()
